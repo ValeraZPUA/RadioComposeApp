fc4293ff0e807ff14ac171acfe872f0d3212f739
Valery Lyuty <lvv0490@gmail.com>
2023-06-01 13:12:10 +0300

    show UI

--- app/src/main/java/com/example/radiocomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/radiocomposeapp/MainActivity.kt
@@ -10,31 +10,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.radiocomposeapp.ui.screens.CountryListScreen
 import com.example.radiocomposeapp.ui.theme.RadioComposeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RadioComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CountryListScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RadioComposeAppTheme {
-        Greeting("Android")
     }
 }
