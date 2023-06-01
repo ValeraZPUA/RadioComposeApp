b78459de50fc95def8ac87d60fc3135e7299cb4d
Valery Lyuty <lvv0490@gmail.com>
2023-06-01 16:06:13 +0300

    added country ui states

--- app/src/main/java/com/example/radiocomposeapp/ui/viewmodels/CountryViewModel.kt
+++ app/src/main/java/com/example/radiocomposeapp/ui/viewmodels/CountryViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.radiocomposeapp.ui.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.radiocomposeapp.data.repository.CountryRepository
 import com.example.radiocomposeapp.data.room.Country
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,6 +19,26 @@ class CountryViewModel @Inject constructor(
 ) : ViewModel() {
 
     val countries: Flow<List<Country>> = repository.getCountries()
+
+    var countryUiState: CountryUiState by mutableStateOf(CountryUiState.Loading)
+        private set
+
+    init {
+        getCountries()
+    }
+
+    private fun getCountries() {
+        viewModelScope.launch {
+            countryUiState = CountryUiState.Loading
+
+            countryUiState = try {
+                val countries = repository.getCountries().first()
+                CountryUiState.Success(countries)
+            } catch (e: Exception) {
+                CountryUiState.Error
+            }
+        }
+    }
 
     fun selectCountry(country: Country) {
         country.isSelected = true
@@ -29,4 +53,10 @@ class CountryViewModel @Inject constructor(
             repository.removeSelectedCountry(country)
         }
     }
+}
+
+sealed class CountryUiState {
+    data class Success(val countries: List<Country>) : CountryUiState()
+    object Error: CountryUiState()
+    object Loading : CountryUiState()
 }
